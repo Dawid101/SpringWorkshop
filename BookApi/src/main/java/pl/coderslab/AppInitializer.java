bd636a851c94f03103b213545c39ed54bfde571c
dawid101 <dawcik35@gmail.com>
2025-05-25 15:52:33 +0200

    konfiguracja

--- BookApi/src/main/java/pl/coderslab/AppInitializer.java
+++ BookApi/src/main/java/pl/coderslab/AppInitializer.java
@@ -2,19 +2,17 @@ package pl.coderslab;
 
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+public class AppInitializer extends
+        AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
-    protected Class<?>[] getRootConfigClasses() {
-        return null;
-    }
+    protected Class<?>[] getRootConfigClasses() { return null; }
 
     @Override
     protected Class<?>[] getServletConfigClasses() {
-        return new Class[]{AppConfig.class};
-    }
+        return new Class[]{AppConfig.class};  }
 
     @Override
     protected String[] getServletMappings() {
-        return new String[]{"/"};
-    }
+        return new String[]{"/"};   }
 }
+
--- BookApi/src/main/java/pl/coderslab/BookController.java
+++ BookApi/src/main/java/pl/coderslab/BookController.java
@@ -1,6 +1,7 @@
 package pl.coderslab;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,3 +16,4 @@ public class BookController {
                 "Bruce Eckel", "Helion", "programming");
     }
 }
+
